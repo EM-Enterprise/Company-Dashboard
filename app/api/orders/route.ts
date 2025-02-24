e442970c040fc5bfec9a5370fc8942fa0529c4fb
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-02-24 16:24:12 +0100

    ref: added error response for invalid requests getOrders
    The reason for this change is that when the user requests to many orders then the getOrders call would be violated, because the limit must be at most 100. Hence, a error message was introduced preventing this scenario.

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -7,6 +7,8 @@ export async function GET(req: Request) {
   const params = new URL(req.url).searchParams
   const limit = parseInt(params?.get('limit')?.toString() ?? '25')
 
+  if (limit > 100) return new Response('Limit must be less than or equal to 100', { status: 400 })
+
   const { getOrders } = await getWoocommerceApi()
   const orders = await getOrders({ per_page: limit })
 
