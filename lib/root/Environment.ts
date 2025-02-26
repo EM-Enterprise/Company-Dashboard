18e0baf3f1704fac76afa4bd1102e8c430e6b4a1
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-02-26 15:43:48 +0100

    ref: refined environment schema
    The reason for this change is that some properties of the schema are validated in terms of their content, for example urls, to provide a initial safe-guard for wrong inputs.

--- lib/root/Environment.ts
+++ lib/root/Environment.ts
@@ -1,42 +1,55 @@
 import { z } from 'zod'
 
-const props = [
-  'PORT',
-  'PUBLIC_URL',
-  'BACKEND',
-  'DATABASE_HOST',
-  'DATABASE_PORT',
-  'DATABASE_NAME',
-  'MONGODB_URI',
-  'GITHUB_ID',
-  'GITHUB_SECRET',
-  'NEXTAUTH_DB',
-  'NEXTAUTH_URL',
-  'NEXTAUTH_SECRET',
-  'HELLOCASH_TOKEN',
-  'WOOCOMMERCE_DASHBOARD',
-  'WOOCOMMERCE_DOMAIN',
-  'WOOCOMMERCE_CONSUMER_KEY',
-  'WOOCOMMERCE_CONSUMER_SECRET',
-  'WINSTON_SILENT',
-] as const
+export const envSchema = z.object({
+  PORT: z.string().transform((val) => parseInt(val)),
+  PUBLIC_URL: z.string().startsWith('http').includes('://'),
+  API_BACKEND_URL: z.string().startsWith('http').includes('://'),
 
-const schemaShape: Record<string, z.ZodTypeAny> = {}
-props.forEach((key) => {
-  schemaShape[key] = z.string().optional()
+  DATABASE_HOST_IP: z
+    .string()
+    .regex(
+      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
+      'Invalid Database Host IP Address',
+    ),
+  DATABASE_PORT: z.string().transform((val) => parseInt(val)),
+  DATABASE_NAME: z.string(),
+  MONGODB_EXTRA_URI_ARGS: z
+    .string()
+    .regex(/^(?:[^&=]+=[^&=]+)(?:&[^&=]+=[^&=]+)*$/g, "Invalid URI Argument Format. Must be in the form 'key=value' joined by '&'.")
+    .transform((val) => val?.trim() || '')
+    .optional(),
+
+  GITHUB_ID: z.string(),
+  GITHUB_SECRET: z.string(),
+
+  NEXTAUTH_DATABASE_NAME: z.string(),
+  NEXTAUTH_URL: z.string().startsWith('http').includes('://'),
+  NEXTAUTH_SECRET: z.string(),
+
+  HELLOCASH_TOKEN: z.string(),
+
+  WOOCOMMERCE_DASHBOARD_BASE_URL: z.string().startsWith('http').includes('://'),
+  WOOCOMMERCE_API_DOMAIN: z.string().startsWith('http').includes('://'),
+  WOOCOMMERCE_CONSUMER_KEY: z
+    .string()
+    .startsWith('ck_', { message: 'consumerKey must start with "ck_"' })
+    .min(10, { message: 'consumerKey must be at least 10 characters long' }),
+  WOOCOMMERCE_CONSUMER_SECRET: z
+    .string()
+    .startsWith('cs_', { message: 'consumerSecret must start with "cs_"' })
+    .min(10, { message: 'consumerSecret must be at least 10 characters long' }),
+
+  WINSTON_SILENT: z
+    .string()
+    .optional()
+    .transform((val) => val?.toLowerCase().trim() === 'true')
+    .default('false'),
 })
 
-const schema = z.object(schemaShape).superRefine((data, ctx) => {
-  for (let prop of props) {
-    if (!data.hasOwnProperty(prop) || !data[prop])
-      ctx.addIssue({
-        code: 'not_finite',
-        message: `${prop} is missing`,
-      })
-  }
-})
+const res = envSchema.safeParse(process.env)
+if (res.error) {
+  throw new Error(`Missing Environment Variables: \n ${JSON.stringify(res.error.flatten().fieldErrors, null, 2)}`)
+}
 
-const env = schema.parse(process.env) as Record<(typeof props)[number], string>
+const env = envSchema.parse(process.env)
 export default env
-
-export function validateEnv() {}
